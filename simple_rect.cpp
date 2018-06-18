29ced4ee3c107f979875f8dd69fd38e3fb5208b8
ErikBoesen <me@erikboesen.com>
2018-06-18 11:55:04 -0400

    Add simple_rect.cpp

---
+++ simple_rect.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Rectangle {
+public:
+    Rectangle(int width, int height) {
+        this->width = width;
+        this->height = height;
+    }
+    void setW(int width) { this->width = width; }
+    void setH(int height) { this->height = height; }
+    int getArea() { return this->width * this->height; }
+    int getPerimeter() { return 2*(this->width + this->height); }
+private:
+    int width, height;
+};
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) return 1;
+    std::cout << "argc: " << argc << std::endl;
+    Rectangle r((int)argc, atoi(argv[1]));
+    std::cout << r.getPerimeter() << std::endl;
+}
